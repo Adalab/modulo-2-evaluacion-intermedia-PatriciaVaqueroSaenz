469a3dab12613b2eeb67c926d8d79d965a50c13c
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-12 09:59:08 +0200

    add function checknum

--- js/main.js
+++ js/main.js
@@ -5,15 +5,38 @@ const inputNumber = document.querySelector('.js-inputNumber');
 const counter = document.querySelector('.js-try');
 const text = document.querySelector('.js-text');
 const randomNumber = getRandomNumber(100);
-
+let cont = 0;
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
-  }
+}
+
+function checkNum(){
+    
+    const num = parseInt(inputNumber.value);
+    
+    if(num < 1 || num > 100){
+        text.innerHTML = "El número debe estar entre 1 y 100" 
+    }
+    else if(num < randomNumber){
+        text.innerHTML = "Pista: Demasiado bajo"; 
+
+    }else if (num > randomNumber){
+        text.innerHTML = "Pista: Demasiado alto"; 
+
+    }else if (num === randomNumber){
+        text.innerHTML = "Has ganado, campeona!!!"; 
+    }else{
+        text.innerHTML = "El número debe estar entre 1 y 100"; 
+    }
+
+}
 
 function handleClickButton(){
     //comprobar numero
+    checkNum();
     //aumentar contador
+    counter();
 }
 
 console.log('Random number: ' + randomNumber);
