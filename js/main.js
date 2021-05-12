2c9f15e2e26178dcbb86a887ae0576ac6a739897
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-12 09:43:56 +0200

    add generate randomnumber

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 'use strict';
 
-const button = document.querySelector('.js-button')
+const button = document.querySelector('.js-button');
+const inputNumber = document.querySelector('.js-inputNumber');
+const counter = document.querySelector('.js-try');
+const text = document.querySelector('.js-text');
+const randomNumber = getRandomNumber(100);
 
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
   }
 
+function handleClickButton(){
+    //comprobar numero
+    //aumentar contador
+}
 
+console.log('Random number: ' + randomNumber);
 
-
-
-button.addEventListener('click',handler);
+button.addEventListener('click',handleClickButton);
