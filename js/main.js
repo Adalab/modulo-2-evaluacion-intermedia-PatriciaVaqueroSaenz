a05ceb1b724577708d23b1224a56e2c07d431350
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-05-12 09:07:41 +0200

    add HTML structure

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const button = document.querySelector('.js-button')
+
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+  }
+
+
+
+
+
+button.addEventListener('click',handler);
